bc4d0f7f488379267d4bc1affb2e732139fee2cc
eduardo escobar <war_army_10@hotmail.com>
2024-09-20 00:16:53 -0600

    create basic scheduler job structure

--- src/job.rs
+++ src/job.rs
@@ -1,8 +1,66 @@
+use std::fmt::{Display, Formatter};
 use std::time::Duration;
-use chrono::Utc;
+use chrono::{Utc, DateTime};
+
+#[derive(Debug, Clone)]
 pub struct Job {
     name: String,
-    expire: Utc,
+    expire: DateTime<Utc>,
     interval: Duration,
-    task: Box<dyn Fn() + Send + 'static>,
+    message: String,
+}
+
+#[derive(Debug, Clone)]
+enum JobError {
+    ExpirationInPast(DateTime<Utc>),
+}
+
+impl Display for JobError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        writeln!(f,"Datetime Error:")
+    }
+}
+
+impl Job {
+    pub fn new(name: &str, expire: DateTime<Utc>, interval: Duration, message: &str) -> Result<Job, JobError> {
+        if !Self::is_greater_than_now(expire) {
+            Err(JobError::ExpirationInPast(expire))
+        } else {
+            Ok(Self {
+                name: name.to_string(),
+                expire,
+                interval,
+                message: message.to_string(),
+            })
+        }
+    }
+
+    pub fn name(&self) -> &str {
+        &self.name
+    }
+
+    pub fn expire(&self) -> DateTime<Utc> {
+        self.expire
+    }
+
+    pub fn interval(&self) -> Duration {
+        self.interval
+    }
+
+    pub fn message(&self) -> &str  {
+        &self.message
+    }
+
+    pub fn execute(&self) {
+        println!("{} {}",Utc::now(), self.message);
+    }
+
+    pub fn has_expired(&self) -> bool {
+        self.expire < Utc::now()
+    }
+
+    fn is_greater_than_now(datetime: DateTime<Utc>) -> bool {
+        datetime > Utc::now()
+    }
+
 }
