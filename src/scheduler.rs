b8f6cdd91a92a0739ea0a635719a5800a8054c3f
eduardo escobar <war_army_10@hotmail.com>
2024-09-24 00:00:42 -0600

    Implementing Scheduler Structure and functions

--- src/scheduler.rs
+++ src/scheduler.rs
@@ -1,18 +1,36 @@
 use std::collections::HashMap;
+use std::error::Error;
+use std::fmt::{Display, Formatter};
 use crate::job::Job;
 
 pub struct Scheduler {
     jobs: HashMap<String, Job>,
 }
 
+#[derive(Debug, Clone)]
+enum SchedulerError {
+    JobAlreadyExists(Job)
+}
+
+impl Display for SchedulerError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        unimplemented!("Display for Scheduler Error")
+    }
+}
+
+impl Error for SchedulerError {
+
+}
+
 impl Scheduler {
+
     pub fn new() -> Self {
         Scheduler {
             jobs: HashMap::new()
         }
     }
 
-    pub fn add_job() {
+    pub fn add_job(&mut self, job: Job) -> Result<(), Error> {
         unimplemented!("add_job")
     }
 
@@ -20,5 +38,11 @@ impl Scheduler {
         unimplemented!("remove_job")
     }
 
-    
+    pub fn get_job() {
+        unimplemented!("get_job")
+    }
+
+    pub fn run() {
+        unimplemented!("run")
+    }
 }
