b16f0ab1438ac369774f6e9a1fbda99f88061615
eduardo escobar <war_army_10@hotmail.com>
2024-09-25 23:32:42 -0600

    adding more details for scheduler

--- src/scheduler.rs
+++ src/scheduler.rs
@@ -1,8 +1,9 @@
 use std::collections::HashMap;
 use std::error::Error;
 use std::fmt::{Display, Formatter};
+use chrono::Utc;
 use crate::job::Job;
-use crate::scheduler::SchedulerError::JobAlreadyExists;
+
 
 pub struct Scheduler {
     jobs: HashMap<String, Job>,
@@ -10,12 +11,18 @@ pub struct Scheduler {
 
 #[derive(Debug, Clone)]
 enum SchedulerError {
-    JobAlreadyExists(Job)
+    JobAlreadyExists(String),
+    JobDoesntExists(String),
 }
 
 impl Display for SchedulerError {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        unimplemented!("Display for Scheduler Error")
+        match self {
+            SchedulerError::JobAlreadyExists(job) =>
+                writeln!(f, "Job {} already exists", job),
+            SchedulerError::JobDoesntExists(job_name) =>
+                writeln!(f, "Job {} does not exists", job_name)
+        }
     }
 }
 
@@ -33,22 +40,41 @@ impl Scheduler {
 
     pub fn add_job(&mut self, job: Job) -> Result<(), SchedulerError> {
         if self.jobs.contains_key(job.name()) {
-            Err(JobAlreadyExists(job))
+            Err(SchedulerError::JobAlreadyExists(job.name().to_string()))
         } else {
             self.jobs.insert(job.name().to_string(), job);
             Ok(())
         }
     }
 
-    pub fn remove_job() {
-        unimplemented!("remove_job")
+    pub fn remove_job(&mut self, job_name: &str) -> Result<Job, SchedulerError>{
+        if !self.jobs.contains_key(job_name) {
+            Err(SchedulerError::JobDoesntExists(job_name.to_string()))
+        } else {
+            Ok(self.jobs.remove(job_name).unwrap())
+        }
     }
 
-    pub fn get_job() {
-        unimplemented!("get_job")
+    pub fn get_job(&self, job_name: &str) -> Option<&Job> {
+        self.jobs.get(job_name)
     }
 
-    pub fn run() {
-        unimplemented!("run")
+    pub fn run(&mut self) {
+        let mut jobs_to_remove = Vec::new();
+
+        for (name, job) in self.jobs.iter_mut() {
+            if job.is_due() {
+                job.execute();
+            }
+
+            if job.should_remove() {
+                jobs_to_remove.push(name.clone());
+            }
+        }
+
+        // Remove expired jobs
+        for name in jobs_to_remove {
+            self.jobs.remove(&name);
+        }
     }
 }
